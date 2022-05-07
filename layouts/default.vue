7fd0e22bfa225d09551e0564f9a5aaca03b671d0
stxarixdust <ari@llamanet.de>
2022-05-07 12:14:52 +0200

    Add basic global style rules

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,3 @@
-/* eslint-disable vue/multi-word-component-names */
 <template>
   <div>
     <TopHeader/>
@@ -10,5 +9,20 @@
   body {
     background: #DFE6E9;
     margin: 0;
+    font-size: 18px;
+    font-family: "Apoc LC Trial Light";
+  }
+  h1{
+    font-family: "Attack Type";
+    font-weight: 500;
+    font-size: 91.13px;
+    letter-spacing: -4px;
+    line-height: 85%;
+    margin: 0;
+  }
+  p{
+    line-height: 1.6;
+    margin: 0;
+    text-align: justify;
   }
 </style>
